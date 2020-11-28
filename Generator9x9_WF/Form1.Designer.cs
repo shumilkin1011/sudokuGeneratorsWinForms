2f586bcd07ccdeb681a66d0e43c0debebc2f1da7
Sergey <shumilkin51@list.ru>
2020-11-28 15:29:58 +0300

    Initial Commit

---
+++ Generator9x9_WF/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Generator9x9_WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(167, 206);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(156, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.ShowCellErrors = false;
+            this.dataGridView1.ShowCellToolTips = false;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.ShowRowErrors = false;
+            this.dataGridView1.Size = new System.Drawing.Size(200, 181);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.SizeChanged += new System.EventHandler(this.dataGridView2_SizeChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Coral;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(17, 267);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(356, 95);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "СГЕНЕРИРОВАТЬ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(244, 241);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(79, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AllowUserToResizeColumns = false;
+            this.dataGridView2.AllowUserToResizeRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView2.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.ColumnHeadersVisible = false;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView2.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView2.Location = new System.Drawing.Point(241, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView2.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView2.ShowCellErrors = false;
+            this.dataGridView2.ShowCellToolTips = false;
+            this.dataGridView2.ShowEditingIcon = false;
+            this.dataGridView2.ShowRowErrors = false;
+            this.dataGridView2.Size = new System.Drawing.Size(200, 181);
+            this.dataGridView2.TabIndex = 5;
+            this.dataGridView2.SizeChanged += new System.EventHandler(this.dataGridView2_SizeChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(60, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Сложность";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(60, 241);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Количество уровней";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Maroon;
+            this.label3.Location = new System.Drawing.Point(69, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(335, 51);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "ГЕНЕРАЦИЯ...";
+            this.label3.Visible = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.listBox1.Location = new System.Drawing.Point(379, 202);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(62, 160);
+            this.listBox1.TabIndex = 10;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.ClientSize = new System.Drawing.Size(459, 371);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SudokuGenerator 9x9";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Generator9x9_WF/Program.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Generator9x9_WF
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+
+
+    public enum DifficultyLevel { EASY_9x9 = 38, MEDIUM_9x9 = 30, HARD_9x9 = 25, EXPERT_9x9 = 23, NORMAL_16x16 = 121 };
+    public enum SudokuSize { SIZE_9x9 = 9, SIZE_16x16 = 16 };
+    [Serializable]
+    public struct LevelInfo
+    {
+        public int[] tiles;
+        public SudokuSize size;
+        public DifficultyLevel difficulty;
+        public int[] closedTiles;
+        public long elapsedTime;
+        public long mistakes;
+
+
+        //public DifficultyLevel Difficulty { get { return difficulty; } set{ difficulty = value; } }
+        //public SudokuSize Size { get { return size; } set { size = value; } }
+        //public int[] Tiles { get { return tiles; } set { tiles = value; } }
+        //public int[] ClosedTiles { get { return closedTiles; } set { closedTiles = value; } }
+        //public long ElapsedTime { get { return elapsedTime; } set { elapsedTime = value; } }
+        //public long Mistakes { get { return mistakes; } set { mistakes = value; } }
+    }
+
+    public class Generator
+    {
+        private int N;
+        private int[,] field;
+        private int[,] completedField;
+        private DifficultyLevel difficulty;
+        System.IO.StreamWriter file;
+
+        public int[,] Field
+        {
+            get { return field; }
+        }
+
+        public int[,] CompletedField
+        {
+            get { return completedField; }
+        }
+
+        public int Size
+        {
+            get { return N; }
+        }
+
+        string path;
+
+        public string FilePath
+        {
+            get { return path; }
+        }
+
+
+        private static Generator generator = null;
+        Random rnd = new Random();
+        private bool find = false;
+
+        public Generator() { }
+
+        public void Generate(DifficultyLevel lvl, SudokuSize sudokuSize, int _count)
+        {
+            path = "LEVELS_" + lvl + "_" + DateTime.Now.Hour + "-" + DateTime.Now.Minute + "-" + DateTime.Now.Millisecond;
+            file = new System.IO.StreamWriter(path, true);
+            difficulty = lvl;
+            this.N = (int)sudokuSize;
+
+
+            int counter = 0;
+            int count = _count;
+            while (counter < count)
+            {
+                while (!find)
+                {
+                    field = new int[N, N];
+                    completedField = new int[N, N];
+                    GenerateField();
+                    completedField = (int[,])field.Clone();
+                    CloseTiles_2();
+
+                }
+
+                WriteToFile();
+                find = false;
+                counter++;
+
+            }
+            file.Close();
+        }
+
+        private void WriteToFile()
+        {
+
+            LevelInfo info = new LevelInfo();
+            info.difficulty = this.difficulty;
+            info.size = (SudokuSize)this.N;
+            info.closedTiles = new int[N * N];
+            info.tiles = new int[N * N];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    info.tiles[i * N + j] = this.completedField[i, j];
+                    info.closedTiles[i * N + j] = this.field[i, j];
+                }
+            }
+
+            string s = Newtonsoft.Json.JsonConvert.SerializeObject(info);
+            file.WriteLine(s);
+            file.Flush();
+        }
+
+
+        private bool CheckRules(int i, int j, int num)
+        {
+
+            if (CheckRow(i, num) && CheckColumn(j, num) && CheckBox(i, j, num)) return true;
+            else return false;
+
+        }
+
+        bool CheckRow(int i, int num)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (field[i, j] == num) return false;
+            }
+            return true;
+
+        }
+
+        bool CheckColumn(int j, int num)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                if (field[i, j] == num) return false;
+            }
+            return true;
+        }
+
+        bool CheckBox(int i, int j, int num)
+        {
+            int temp = 3;
+            if (N == (int)SudokuSize.SIZE_9x9) temp = 3;
+            else if (N == (int)SudokuSize.SIZE_16x16) temp = 4;
+
+            int boxCoordX = (i) / temp;
+            int boxCoordY = (j) / temp;
+
+            int startRow = boxCoordX * temp;
+            int startColumn = boxCoordY * temp;
+
+            for (int k = startRow; k < startRow + temp; k++)
+            {
+                for (int m = startColumn; m < startColumn + temp; m++)
+                {
+                    if (field[k, m] == num) return false;
+                }
+            }
+            return true;
+        }
+
+        private bool TryNumber(int i, int j)
+        {
+            List<int> numbers = new List<int> { };
+            for (int k = 1; k <= N; k++) numbers.Add(k);
+            while (numbers.Count > 0)
+            {
+                int num = numbers[rnd.Next(0, numbers.Count)];
+                numbers.Remove(num);
+                if (CheckRules(i, j, num))
+                {
+                    field[i, j] = num;
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+
+        int solve(int i, int j, int[,] cells, int count /*initailly called with 0*/)
+        {
+            if (i == N)
+            {
+                i = 0;
+                if (++j == N)
+                    return 1 + count;
+            }
+            if (cells[i, j] != 0)  // skip filled cells
+                return solve(i + 1, j, cells, count);
+            // search for 2 solutions instead of 1
+            // break, if 2 solutions are found
+            for (int val = 1; val <= N && count < 2; ++val)
+            {
+                if (CheckRules(i, j, val))
+                {
+                    cells[i, j] = val;
+                    // add additional solutions
+                    count = solve(i + 1, j, cells, count);
+                }
+            }
+            cells[i, j] = 0; // reset on backtrack
+            return count;
+
+        }
+
+
+        private void CloseTiles_2()
+        {
+            int[,] tried = new int[N, N];
+            int counter = 0;
+            int difficult = N * N;
+            int temp = 0;
+            while (counter < (N * N) && (difficult >= (int)difficulty || difficulty==DifficultyLevel.HARD_9x9))
+            {
+                Console.WriteLine("counter " + counter);
+                Console.WriteLine("diff " + difficult);
+                Console.WriteLine();
+                int i = rnd.Next(0, N);
+                int j = rnd.Next(0, N);
+
+                if (tried[i, j] == 0)
+                {
+                    counter++;
+                    tried[i, j] = 1;
+                    temp = field[i, j];
+                    field[i, j] = 0;
+                    difficult--;
+
+                    if (solve(0, 0, field, 0) != 1)
+                    {
+                        field[i, j] = temp;
+                        difficult++;
+                    }
+                }
+            }
+            if (difficult <= (int)difficulty) find = true;
+
+            Console.WriteLine("FIELD");
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(completedField[i, j] + " "); ;
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("PUZZLE");
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(field[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Сложность: " + difficult);
+
+        }
+        /*private void CloseTiles()
+        {
+            Console.WriteLine("test4");
+            int actuallyClosed = 0;
+            int closed = N * N - (int)difficulty;
+            int tries = 0;
+            while (actuallyClosed < closed)
+            {
+
+                int rand = rnd.Next(0, N * N);
+                int i = rand / N;
+                int j = rand - N * i;
+
+                int temp = field[i, j];
+                int test = 0;
+                if ((field[i, j] != 0) && ( (test =CheckSolutions(i, j)) == 1))
+                {
+                    actuallyClosed++;
+                    Console.WriteLine("ТЕСТ" + actuallyClosed + "/" + closed + "\nSolutions=" + test);
+                    Console.WriteLine("Открыто" + (N*N-actuallyClosed) + "/" + (N*N));
+                    for (int c = 0; c < N; c++)
+                    {
+                        for (int a = 0; a < N; a++)
+                        {
+                            Console.Write("{0,2} ", field[c, a]);
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine();
+                    }
+                    tries = 0;
+
+                }
+                else
+                {
+                    field[i, j] = temp;
+
+                    tries++;
+                    if(tries > 2500)
+                    {
+                        if(actuallyClosed > 40)
+                        {
+                            if (actuallyClosed >= 50) s.Write("!!!WIN!!!----->>>");
+                            s.WriteLine(actuallyClosed);
+                            s.Flush();
+
+                        }
+                        find = false;
+                        return;
+                    }
+
+                }
+            }
+            find = true;
+            for(int i =0;i<N;i++)
+            {
+                for(int j = 0;j<N;j++)
+                {
+                    Console.Write(field[i, j] + " ");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }*/
+
+        private void GenerateField()
+        {
+            int[,] temp = (int[,])field.Clone();
+            int k = 0;
+            while (k < (N * N))
+            {
+                int i = k / N;
+                int j = k - N * i;
+
+                if (!TryNumber(i, j))
+                {
+                    temp[i, j]++;
+                    int koef = temp[i, j] / 6;
+                    for (int c = k; c >= (k - 1 - koef); c--)
+                    {
+                        i = c / N;
+                        j = c - N * i;
+                        field[i, j] = 0;
+                    }
+
+                    k = k - 1 - koef;
+                    if (k < 0) k = 0;
+                }
+                else
+                {
+                    k++;
+                }
+            }
+        }
+    }
+
+
+}
